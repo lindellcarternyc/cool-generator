9f3c5c5888386b8a9673901528ff4c19e3bcc7dd
Lindell Carter <lindellcarternyc@gmail.com>
2021-08-14 08:55:34 -0400

    adds context and controls to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,20 @@
+import { SettingsProvider } from './context/SettingsContext'
+
+import Controls from './components/Controls/Controls'
+import Output from './components/Output/Output'
+
 const App = () => {
   return (
-    <div>Colors</div>
+    <SettingsProvider>
+      <main style={{
+        textAlign: 'center',
+        padding: '64px'
+      }}>
+        <h1>Animated CSS Gradient Background Generator</h1>
+        <Controls />
+        <Output />
+      </main>
+    </SettingsProvider>
   )
 }
 
