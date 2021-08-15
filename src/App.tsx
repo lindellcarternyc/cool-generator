8f38ed30f36abe867e8e5fad3bfcc440b921f8a3
Lindell Carter <lindellcarternyc@gmail.com>
2021-08-15 08:39:55 -0400

    adds animated background to app

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,24 @@ import AddColor from './components/AddColor/AddColor'
 import Output from './components/Output/Output'
 import Angle from './components/Sliders/AngleSlider'
 import Speed from './components/Sliders/SpeedSlider'
+import AnimatedBackground from './components/AnimatedBackground/AnimatedBackground'
 
 const App = () => {
   return (
     <SettingsProvider>
-      <main style={{
-        textAlign: 'center',
-        padding: '64px'
-      }}>
-        <h1>Animated CSS Gradient Background Generator</h1>
-        <Controls />
-        <AddColor />
-        <Angle />
-        <Speed />
-        <Output />
-      </main>
+      <AnimatedBackground>
+        <main style={{
+          textAlign: 'center',
+          padding: '64px'
+        }}>
+          <h1>Animated CSS Gradient Background Generator</h1>
+          <Controls />
+          <AddColor />
+          <Angle />
+          <Speed />
+          <Output />
+        </main>
+      </AnimatedBackground>
     </SettingsProvider>
   )
 }
